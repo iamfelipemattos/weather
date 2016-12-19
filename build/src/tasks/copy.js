213c255c61893c534de5e853091a38def3a7d58b
Felipe Mattos <hi@felipemattos.io>
2016-12-18 22:23:38 -0200

    Deletes old assets

--- build/src/tasks/copy.js
+++
@@ -1,4 +0,0 @@
-gulp.task('copy', function () {
-  return gulp.src('index.html')
-    .pipe(gulp.dest('dist/'));
-});
--- build/tasks/copy.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-
-gulp.task('copy', () => {
-  return gulp.src('index.html')
-    .pipe(gulp.dest('../dist/'));
-
-});
